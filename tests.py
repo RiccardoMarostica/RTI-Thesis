2e431927262aa6b46acd59def4744ff4f17cdc03
Riccardo Marostica <46026232+RiccardoMarostica@users.noreply.github.com>
2023-04-24 17:39:20 +0200

    Added homography H21 and recover camera pose for camera 1 and 2

--- tests.py
+++ tests.py
@@ -3,7 +3,6 @@ import numpy as np
 
 from constants import *
 
-
 # Load the two videos and extract their audio
 # Frame difference: 82 First video start sooner
 video1 = cv.VideoCapture(STATIC_VIDEO_FILE_PATH)
@@ -13,7 +12,7 @@ video1.set(cv.CAP_PROP_FPS, 30)
 video2.set(cv.CAP_PROP_FPS, 30)
 
 video1.set(cv.CAP_PROP_POS_FRAMES, 82)
-        
+
 points = []
 
 # TEST WITH FIDUAL MARKER FIND VIA MOUSE POINTS
@@ -33,6 +32,27 @@ def getPointFromImage(event, x, y, flags, params):
         points.append(np.array((x, y), dtype=np.float32))
         print("Point added")
     pass
+
+# H -> Homography
+# K -> Intrinsic Parameters of the camera
+# The function computes the extrinsic parameters using the homography and intrinsic parameters array.
+# At the end, it returns the derived rotation and translation vectors
+
+
+def findCameraExtrinsicsParameters(H, K):
+    H = H.T                                         # Get the transpose of the Homography
+    K_inverse = np.linalg.inv(K)                    # Get the inverse of the intrisinc parameters
+    h1 = H[0]                                       # First column of the Homography
+    h2 = H[1]                                       # Second column
+    h3 = H[2]                                       # Third column
+    L = 1 / np.linalg.norm(np.dot(K_inverse, h1))   # Scale factor
+    r1 = L * np.dot(K_inverse, h1)                  #  Rotation matrix first column
+    r2 = L * np.dot(K_inverse, h2)                  #  Rotation matrix second column
+    r3 = np.cross(r1, r2)                           #  Rotation matrix third column
+    T = L * (K_inverse @ h3.reshape(3, 1))          # Get the translation vector
+    R = np.array([[r1], [r2], [r3]])                #  Get the rotation matrix
+    R = np.reshape(R, (3, 3))
+    return R, T                                     # Return extrinsic parameters
 
 
 while video1.isOpened() and video2.isOpened():
@@ -65,14 +85,14 @@ while video1.isOpened() and video2.isOpened():
             # Set the destination points for the real world.
             # In this case we are setting to project the image into a square of 480x480px
             destinationPoints = np.array([
-                [0, 0], 
-                [DEFAULT_ASPECT_RATIO, 0], 
-                [DEFAULT_ASPECT_RATIO, DEFAULT_ASPECT_RATIO], 
+                [0, 0],
+                [DEFAULT_ASPECT_RATIO, 0],
+                [DEFAULT_ASPECT_RATIO, DEFAULT_ASPECT_RATIO],
                 [0, DEFAULT_ASPECT_RATIO]
             ])
 
             # Now we compute the Homography between the World and the Static Camera
-            homographyStaticCamera = cv.findHomography(np.array(points), destinationPoints)
+            homographyStaticCamera, _ = cv.findHomography(np.array(points), destinationPoints)
 
             # Set that the key points has been found and stored
             hasFoundPoints = True
@@ -89,74 +109,61 @@ while video1.isOpened() and video2.isOpened():
         cv.destroyWindow("Point detection")
         hasDestroyedPointsWindow = True
 
-    # Now, since we have the Homography between World and Static Camera, we can show both of them
-    # First, let's get the Region Of Interest (ROI) for the Static Camera
-    x, y = points[0]
-    
-    # Define the ROI, using x and y coordinates of the first point
-    roi_x, roi_y, roi_w, roi_h = int(x), int(y), DEFAULT_ASPECT_RATIO, DEFAULT_ASPECT_RATIO
-    roi = np.copy(staticFrame)
-    roi = roi[roi_y : (roi_y + roi_h), roi_x : (roi_x + roi_w)]
-    
     # Define world camera
-    world = cv.warpPerspective(staticFrame, homographyStaticCamera[0], (DEFAULT_ASPECT_RATIO, DEFAULT_ASPECT_RATIO))
-    
+    world = cv.warpPerspective(staticFrame, homographyStaticCamera, (DEFAULT_ASPECT_RATIO, DEFAULT_ASPECT_RATIO))
+
+    # # Get the intrinsic parameters from static video
+    # mtxVideoStatic = np.loadtxt(STATIC_VIDEO_PARAMS_PATH + "intrinsicMatrix.dat")
+    # distVideoStatic = np.loadtxt(STATIC_VIDEO_PARAMS_PATH + "distortionCoeffs.dat")
+
+    # # ... and retrieve extrinsic parameters
+    # rvec, tvec = findCameraExtrinsicsParameters(homographyStaticCamera[0], mtxVideoStatic)
+
+    # axis = np.float32([[3,0,0], [0,3,0], [0,0,-3]]).reshape(-1,3)
+    # imgpts, _ = cv.projectPoints(axis, rvec, tvec, mtxVideoStatic, distVideoStatic)
+
     # SIFT MATCH BETWEEN ROI AND MOVING CAMERA
     # Now, let's try to compute the features of each pov
-    kpROI, desROI = sift.detectAndCompute(roi, None)
+    kpStatic, desStatic = sift.detectAndCompute(world, None)
     kpMoving, desMoving = sift.detectAndCompute(movingFrame, None)
-    
+
     # matches = bf.knnMatch(desROI, desMoving, k = 2)
-    
-    matches = flann.knnMatch(desROI,desMoving,k=2) 
-    
+    matches = flann.knnMatch(desStatic, desMoving, k=2)
+
     goodMatches = []
     for m1, m2 in matches:
-        if m1.distance < 0.5 * m2.distance:
-            # goodMatches.append([m1])
+        if m1.distance < 0.7 * m2.distance:
             goodMatches.append(m1)
-            
+
+    print("Matched points", len(goodMatches))
+
     # Here at this point, after computing the good matches we can try to compute the homography H21 (Moving with Static)
     if len(goodMatches) > 10:
-        
-        srcPoints = np.float32([kpROI[m.queryIdx].pt for m in goodMatches]).reshape(-1, 1, 2)
-        dstPoints = np.float32([kpROI[m.trainIdx].pt for m in goodMatches]).reshape(-1, 1, 2)
-        
+
+        srcPoints = np.float32([kpStatic[m.queryIdx].pt for m in goodMatches]).reshape(-1, 1, 2)
+        dstPoints = np.float32([kpStatic[m.trainIdx].pt for m in goodMatches]).reshape(-1, 1, 2)
+
         # # Now we compute the Homography between the Moving and Static Camera
-        homographyStaticMoving, mask = cv.findHomography(srcPoints, dstPoints, cv.RANSAC, 5.0)
-        
-        movingFrame = cv.warpPerspective(movingFrame, homographyStaticMoving[0], (DEFAULT_ASPECT_RATIO, DEFAULT_ASPECT_RATIO))
+        homographyStaticMoving, _ = cv.findHomography(srcPoints, dstPoints)
+
+        if homographyStaticMoving is not None:
+            # Get the distortion params from the file (for moving camera)
+            distVideoMoving = np.loadtxt(MOVING_VIDEO_PARAMS_PATH + "distortionCoeffs.dat")
+            mtxVideoMoving = np.loadtxt(MOVING_VIDEO_PARAMS_PATH + "intrinsicMatrix.dat")
             
-    # siftMatches = cv.drawMatchesKnn(roi, kpROI, movingFrame, kpMoving, goodMatches, None, flags = 2)
-    siftMatches = cv.drawMatches(roi, kpROI, movingFrame, kpMoving, goodMatches, None, flags = 2)
-    
+            # ... and retrieve extrinsic parameters
+            rvec, tvec = findCameraExtrinsicsParameters(homographyStaticMoving, mtxVideoMoving)
+            
+            axis = np.float32([[3, 0, 0], [0, 3, 0], [0, 0, -3]]).reshape(-1, 3)
+            imgpts, _ = cv.projectPoints(axis, rvec, tvec, mtxVideoMoving, distVideoMoving)
+            
+            # img = cv.line(movingFrame, (0,0), tuple(imgpts[0].ravel().astype(int)), (255,0,0), 5)
+            # img = cv.line(movingFrame, (0,0), tuple(imgpts[1].ravel().astype(int)), (0,255,0), 5)
+            # img = cv.line(movingFrame, (0,0), tuple(imgpts[2].ravel().astype(int)), (0,0,255), 5)
+
+    siftMatches = cv.drawMatches(world, kpStatic, movingFrame, kpMoving, goodMatches, None, flags=2)
+
     cv.imshow("SIFT Matches", siftMatches)
-    
-    
-    # SIFT MATCH BETWEEN ROI AND WORLD
-    # # Put the text to understand
-    # cv.putText(roi, "ROI", (30, 30), fontFace = cv.FONT_HERSHEY_SIMPLEX, fontScale = 1, color = (0, 255, 0), thickness = 2)
-    # # Put the text to understand
-    # cv.putText(world, "World", (30, 30), fontFace = cv.FONT_HERSHEY_SIMPLEX, fontScale = 1, color = (0, 255, 0), thickness = 2)
-    
-    # # Now, let's try to compute the features of each pov
-    # kpROI, desROI = sift.detectAndCompute(roi, None)
-    # kpWorld, desWorld = sift.detectAndCompute(world, None)
-    
-    # matches = bf.knnMatch(desROI, desWorld, k = 2)
-    
-    # goodMatches = []
-    # for m1, m2 in matches:
-    #     if m1.distance < 0.6 * m2.distance:
-    #         goodMatches.append([m1])
-            
-    # siftMatches = cv.drawMatchesKnn(roi, kpROI, world, kpWorld, goodMatches, None, flags = 2)
-    
-    # cv.imshow("SIFT Matches", siftMatches)
-    
-    # img2show = np.concatenate((roi, world), axis = 1)
-    # cv.imshow("Frame", img2show)
-    
 
     # Press Q on the keyboard to exit.
     if (cv.waitKey(25) & 0xFF == ord('q')):
