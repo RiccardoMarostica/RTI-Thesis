4ec212cf9804efb1adf3850abfb7c9546709cb17
Riccardo Marostica <46026232+RiccardoMarostica@users.noreply.github.com>
2023-08-06 13:47:47 +0200

    Removed function folder

--- functions/helpers.py
+++
@@ -1,4 +0,0 @@
-def consoleLog(message: str, method: str, isError: bool = False):
-    colorEscape = "\033[91m" if isError else "\033[1;32m"
-    
-    print(colorEscape + "[CONSOLE - " + method.upper() + "]\033[0m", message)
